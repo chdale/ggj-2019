6e91bb94e4a43b99b631c53dc9c5daa4ac53c5f3
ahevenstone <aaron.hevenstone@gmail.com>
2019-01-26 15:47:09 -0500

    Main Menu Updates

---
+++ Hatch/Assets/Scripts/UI/FlexibleUIText.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class FlexibleUIText : FlexibleUI
+
+{
+    private Text text;
+    public bool useMainFont = true;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        base.Initialize();
+    }
+
+    protected override void OnSkinUI()
+    {
+        base.OnSkinUI();
+        text.color = flexibleUIData.textColor;
+        if (useMainFont)
+        {
+            text.font = flexibleUIData.font;
+        }
+        
+    }
+}
