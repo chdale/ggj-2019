73c569cc24e84e387306ccc24c993c0c56d7ac62
Terrynvail <47060544+Terrynvail@users.noreply.github.com>
2019-01-26 15:27:42 -0500

    Wwise integrated
    3 soundbanks added, with the "Tunnel" sounbank added to the camera in the tunnel scene. Title screen and Tunnel sfx included.

---
+++ Hatch/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,47 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BONE_CRACK = 3507305104U;
+        static const AkUniqueID FOOTSTEP = 1866025847U;
+        static const AkUniqueID MENU_AMBIENT = 2594531601U;
+        static const AkUniqueID MENU_HOVER = 309439191U;
+        static const AkUniqueID MENU_SELECT = 4203375351U;
+        static const AkUniqueID OPENING_SFX = 3281176347U;
+        static const AkUniqueID PAUSE_AMBIENT = 952565488U;
+        static const AkUniqueID START_GAME_MUSIC = 1267737896U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID HATCH_HOME = 1273277481U;
+        static const AkUniqueID TITLE_SCREEN = 3853285476U;
+        static const AkUniqueID TUNNEL = 3059984139U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
