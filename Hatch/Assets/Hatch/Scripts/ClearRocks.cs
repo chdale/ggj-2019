c6711fd0ab813093809381e872dbd61f72f1fadf
Unknown <muffinking3@gmail.com>
2019-01-26 22:40:45 -0500

    adding npc animation logic

---
+++ Hatch/Assets/Hatch/Scripts/ClearRocks.cs
@@ -0,0 +1,14 @@
+﻿using Spine.Unity.Examples;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearRocks : InteractEvent {
+
+    public GameObject AnimationObject;
+
+    public override void TriggerEvent()
+    {
+        AnimationObject.GetComponent<AnimationController>().TriggerAnimationsToggle();
+    }
+}
