e24c884fdb507f4e9d4c106aa076082fe62c2f9a
Cotoly <colt_dale@live.com>
2019-02-08 22:41:08 -0500

    photo pickup and lerp to and from fog wall logic + placeholders

---
+++ Hatch/Assets/Hatch/Scripts/PhotoPickup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoPickup : InteractEvent {
+
+    public GameObject photo;
+    public GameObject fogWall;
+    public GameObject overworldPhoto;
+    private PhotoManager photoManager;
+
+	// Use this for initialization
+	void Start () {
+        Subscribe();
+        photoManager = GameObject.Find("PhotoManager").GetComponent<PhotoManager>();
+	}
+
+    public override void TriggerEvent()
+    {
+        overworldPhoto.SetActive(false);
+        gameController.StopCharacter();
+        gameController.InteractInactiveEvent();
+        photoManager.FadePhotoIn(photo, fogWall);
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+}
