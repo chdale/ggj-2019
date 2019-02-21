ebf4fb067a8adc5280cebadc4ddba25f60055540
ponyboi <aidanarrowood@gmail.com>
2019-02-20 20:46:01 -0500

    adding pulsing light script

---
+++ Hatch/Assets/LightParticle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LightParticle : MonoBehaviour {
+    public Light lightComp;
+    public float rate;
+    public float brightness;
+
+    // Use this for initialization
+    void Start () {
+        lightComp = GetComponent<Light>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        lightComp.intensity = brightness * Mathf.Sin(Time.time * rate);
+	}
+}
