78a045b049ce1b18c136bd129e777e436e867a88
aaron-hevenstone <aaron.hevenstone@gmail.com>
2019-02-25 22:42:07 -0500

    Fog Wall Prefab
    Set up for the fog wall.  Includes iTween to shake faces as player gets closer to fog.

---
+++ Hatch/Assets/FogController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FogController : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem fogParticles;
+    [SerializeField]
+    private SpriteRenderer fogWallSmoke;
+    [SerializeField]
+    private SpriteRenderer smallFaces;
+    [SerializeField]
+    private SpriteRenderer bigFaceClosed;
+    [SerializeField]
+    private SpriteRenderer bigFaceOpen;
+
+    private GameObject player;
+
+    private Vector3 shake = new Vector3(0.1f, 0.1f);
+    private float shakeTime = 5f;
+    
+	void Start ()
+	{
+	    if (fogParticles == null)
+	    {
+	        fogParticles = this.GetComponentInChildren<ParticleSystem>();
+        }
+        
+	    player = GameObject.Find("Player_Wireframe");
+	}
+	
+	void Update ()
+	{
+	    FadeFaces(smallFaces, 18f);
+	    FadeFaces(bigFaceClosed, 10f);
+	    FadeFaces(bigFaceOpen, 7f);
+	}
+
+    IEnumerator FadeSpriteRenderer(SpriteRenderer sprite, bool fadeIn)
+    {
+        var color = sprite.color;
+
+        if (fadeIn)
+        {
+            iTween.ShakePosition(sprite.gameObject, shake, shakeTime);
+            while (color.a < 1)
+            {
+                color.a += Time.deltaTime / 1f;
+                sprite.color = color;
+                yield return null;
+            }
+
+            sprite.color = color;
+        }
+        else
+        {
+            while (color.a > 0)
+            {
+                color.a -= Time.deltaTime / 1f;
+                sprite.color = color;
+                yield return null;
+            }
+
+            sprite.color = color;
+        }
+        
+    }
+
+    private void FadeFaces(SpriteRenderer sprite, float renderDistance)
+    {
+        var distance = Vector2.Distance(player.transform.position, transform.position);
+        StartCoroutine(distance < renderDistance
+            ? FadeSpriteRenderer(sprite, true)
+            : FadeSpriteRenderer(sprite, false));
+    }
+
+    public void ClearFogWall()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
