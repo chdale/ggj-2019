61171b3b81336769352de0fe5791f90417ddf7a2
ponyboi <aidanarrowood@gmail.com>
2019-02-27 02:09:43 -0500

    adding rock fall logic

---
+++ Hatch/Assets/Hatch/Scripts/Events/Scenes/Pit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Hatch.Scripts.Events.Scenes.SceneData;
+using Spine.Unity;
+using UnityEngine;
+
+namespace Assets.Hatch.Scripts.Events.Scenes
+{
+    public class Pit : Scene
+    {
+        public GameObject SceneTrigger;
+
+        private MysteryManIntro MysteryManIntroDialogue;
+
+        public override void InitScene()
+        {
+            foreach (KeyValuePair<string, Action> sceneEvent in GetSceneEventMethods(this))
+            {
+                AddHandler(sceneEvent.Key, sceneEvent.Value);
+            }
+        }
+        [SceneEvent]
+        public void StartRockFall()
+        {
+            var sceneTrigger = SceneTrigger.GetComponent<RockFall>();
+            sceneTrigger.RockSceneStart();
+        }
+    }
+}
