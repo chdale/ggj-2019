be993f7cd5dda4bd43dadbe7aa771f36a5546ce1
Cotoly <colt_dale@live.com>
2019-02-28 22:44:41 -0500

    add background subway audio + static in console interior

--- Hatch/Assets/Hatch/Scripts/Events/EventTypes/LevelLoadEvent.cs
+++ Hatch/Assets/Hatch/Scripts/Events/EventTypes/LevelLoadEvent.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityStandardAssets._2D;
 
@@ -9,11 +11,13 @@ public class LevelLoadEvent : TriggeredEvent
     public Vector2 newPlayerPosition;
     public Vector3 newCameraPosition;
     public Level level;
-    public AudioSource levelMusic;
+    public AudioSource[] levelMusic;
+    public AudioSource tasteAudio;
     public bool dynamicCameraHorizontal = false;
     public float cameraLeftThreshold;
     public float cameraRightThreshold;
     public float cameraSize = 10.0f;
+    public bool removeTasteAudio = true;
 
     private void Awake()
     {
@@ -36,10 +40,31 @@ public class LevelLoadEvent : TriggeredEvent
             StaticEvent.LoadLevel(new LevelRequirement(newPlayerPosition, newCameraPosition, level, cameraSize));
         }
 
-        if (levelMusic != null)
+        if (levelMusic.Any())
         {
             ResetSceneMusic();
         }
+        if (tasteAudio != null)
+        {
+            if (!removeTasteAudio)
+            {
+                AddTasteAudio();
+            }
+            else
+            {
+                RemoveTasteAudio();
+            }
+        }
+    }
+
+    private void RemoveTasteAudio()
+    {
+        tasteAudio.Stop();
+    }
+
+    private void AddTasteAudio()
+    {
+        tasteAudio.Play();
     }
 
     public void ResetSceneMusic()
@@ -53,6 +78,9 @@ public class LevelLoadEvent : TriggeredEvent
             }
         }
 
-        levelMusic.Play();
+        foreach (var music in levelMusic)
+        {
+            music.Play();
+        }
     }
 }
